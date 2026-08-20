9e6080b07cd84c2492ad29bb1bfdffaad019d265
Pratik Pandit <pratikpandit099@gmail.com>
2026-08-20 18:08:28 +0530

    Time: 6 ms (47.49%), Space: 82.7 MB (71.03%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -11,16 +11,32 @@ class Solution {
         //     if(count>=2)return nums[i];
         // }
         // return -1;
-      
-        Arrays.sort(nums);
-
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] == nums[i - 1]) {
-                return nums[i];
+        int i=0;
+        while(i<nums.length){
+            int correct = nums[i]-1;
+            if(nums[i] != nums[correct]){
+                swap(nums,i,correct);
+            }else{
+                i++;
             }
         }
+        // it swappinfg the numbers but when already existing number is at the available on the correct place then cyclic sort ignore second occured element
 
+        for(int j=0;j<nums.length;j++){
+            if(nums[j] != j+1){
+                return nums[j];
+
+            }
+        }
         return -1;
+       
+
+      
+       
     }
-}
-    
+     void swap (int nums[],int first,int second){
+            int temp = nums[first];
+            nums[first]=nums[second];
+            nums[second]= temp;
+        }
+}   
