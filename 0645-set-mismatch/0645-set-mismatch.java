fd5424fdce3a2502128149e39ecd68e0c97de996
Pratik Pandit <pratikpandit099@gmail.com>
2026-08-28 23:59:53 +0530

    Time: 15 ms (25.21%), Space: 48.4 MB (6.58%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        Arrays.sort(nums);
+
+        int duplicate = -1;
+        int missing = -1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
+                duplicate = nums[i];
+            }
+            else if (nums[i] > nums[i - 1] + 1) {
+                missing = nums[i - 1] + 1;
+            }
+        }
+
+     
+        if (nums[nums.length - 1] != nums.length) {
+            missing = nums.length;
+        }
+        if (nums[0] != 1) {
+            missing = 1;
+        }
+
+        return new int[]{duplicate, missing};
+    }
+}
