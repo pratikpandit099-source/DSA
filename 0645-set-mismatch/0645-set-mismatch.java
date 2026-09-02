96b2acaf0ca4aaab6946f1e9f1f8593c871f156e
Pratik Pandit <pratikpandit099@gmail.com>
2026-09-03 00:44:45 +0530

    Time: 3 ms (69.09%), Space: 47.5 MB (63.2%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,28 +1,56 @@
 import java.util.Arrays;
 
 class Solution {
-    public int[] findErrorNums(int[] nums) {
-        Arrays.sort(nums);
+     public int[] findErrorNums(int[] nums) {
+    //     Arrays.sort(nums);
+
+    //     int duplicate = -1;
+    //     int missing = -1;
+    //     for (int i = 1; i < nums.length; i++) {
+    //         if (nums[i] == nums[i - 1]) {
+    //             duplicate = nums[i];
+    //         }
+    //         else if (nums[i] > nums[i - 1] + 1) {
+    //             missing = nums[i - 1] + 1;
+    //         }
+    //     }
+
+     
+    //     if (nums[nums.length - 1] != nums.length) {
+    //         missing = nums.length;
+    //     }
+    //     if (nums[0] != 1) {
+    //         missing = 1;
+    //     }
+
+    //     return new int[]{duplicate, missing};
+    // }
 
         int duplicate = -1;
         int missing = -1;
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] == nums[i - 1]) {
-                duplicate = nums[i];
+        int i =0;
+        while(i<nums.length){
+            int correct = nums[i]-1;
+            if(nums[i]!= nums[correct]){
+                swap(nums,i,correct);
+            }else{
+                i++;
             }
-            else if (nums[i] > nums[i - 1] + 1) {
-                missing = nums[i - 1] + 1;
+
+        }
+       
+        for (int j = 0; j <nums.length ; j++) {
+            if(nums[j]!= j+1){
+              duplicate = nums[j];
+              missing= j+1;
             }
         }
+        return new int[]{duplicate,missing};
+    }
 
-     
-        if (nums[nums.length - 1] != nums.length) {
-            missing = nums.length;
-        }
-        if (nums[0] != 1) {
-            missing = 1;
-        }
-
-        return new int[]{duplicate, missing};
+    public  static void swap(int[] nums, int first, int second) {
+        int temp = nums[first];
+        nums[first] = nums[second];
+        nums[second] = temp;
     }
 }
