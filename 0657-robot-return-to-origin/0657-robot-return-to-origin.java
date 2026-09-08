731bd5723051221a99189259448e5b8ba862a47e
Pratik Pandit <pratikpandit099@gmail.com>
2026-09-08 23:42:05 +0530

    Time: 6 ms (49.88%), Space: 45.3 MB (87.9%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.java
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -3,24 +3,19 @@ class Solution {
         int i=0;
         int count1=0;
         int count2=0;
-        int count3=0;
-        int count4=0;
         while(i<moves.length()){
             char ch = moves.charAt(i);
             if(ch=='L'){
                 count1++;
             }else if(ch =='R'){
-                count2++;
+                count1--;
             }else if(ch =='U'){
-                count3++;
+                count2++;
             }else{
-                count4++;
+                count2--;
             }
             i++;
         }
-        if(count1 == count2 && count3 == count4){
-            return true;
-        }
-        return false;
+       return count1 == 0 && count2 == 0;
     }
 }
